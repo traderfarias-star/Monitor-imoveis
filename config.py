8ff0e29121e0f5448edfdbf0add5636fe425f0e2
traderfarias-star <traderfarias@gmail.com>
2026-07-31 11:01:52 -0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,10 @@
+import os
+
+OLX_URLS = [
+    "https://www.olx.com.br/imoveis/venda/estado-sc",
+    "https://www.olx.com.br/imoveis/aluguel/estado-sc",
+]
+
+KEYWORDS_FILTER = []
+
+DB_PATH = "anuncios_vistos.db"
